5995f89d5a07e0310ef0853a9b370cf63afb255a
AlexBaldwinBin <oob6gg3@gmail.com>
2026-03-10 16:52:15 +0200

    Update LaunchActivity.java

--- app/src/main/java/com/termux/qstile/LaunchActivity.java
+++ app/src/main/java/com/termux/qstile/LaunchActivity.java
@@ -2,17 +2,22 @@ package com.termux.qstile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import java.util.Arrays;
 
 public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try {
-            Runtime.getRuntime().exec(new String[]{
+            ProcessBuilder pb = new ProcessBuilder(Arrays.asList(
                 "/data/data/com.termux/files/usr/bin/bash",
-                "-c",
                 "/data/data/com.termux/files/home/scripts/scan_wrapper.sh"
-            });
+            ));
+            pb.environment().put("PATH", 
+                "/data/data/com.termux/files/usr/bin:" +
+                "/data/data/com.termux/files/usr/bin/applets:" +
+                "/system/bin:/system/xbin");
+            pb.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
