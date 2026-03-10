a5521174b453f40ee92fc67214900c6884d307c8
AlexBaldwinBin <oob6gg3@gmail.com>
2026-03-10 15:56:17 +0200

    Update ScanTileService.java

--- app/src/main/java/com/termux/qstile/ScanTileService.java
+++ app/src/main/java/com/termux/qstile/ScanTileService.java
@@ -1,8 +1,10 @@
 package com.termux.qstile;
 
+import android.graphics.drawable.Icon;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.content.Intent;
+import android.util.Log;
 
 public class ScanTileService extends TileService {
 
@@ -11,8 +13,9 @@ public class ScanTileService extends TileService {
         super.onStartListening();
         Tile tile = getQsTile();
         if (tile != null) {
-            tile.setState(Tile.STATE_INACTIVE);
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_tile));
             tile.setLabel("Scan");
+            tile.setState(Tile.STATE_ACTIVE);
             tile.updateTile();
         }
     }
@@ -22,7 +25,8 @@ public class ScanTileService extends TileService {
         super.onTileAdded();
         Tile tile = getQsTile();
         if (tile != null) {
-            tile.setState(Tile.STATE_INACTIVE);
+            tile.setIcon(Icon.createWithResource(this, R.drawable.ic_tile));
+            tile.setState(Tile.STATE_ACTIVE);
             tile.updateTile();
         }
     }
@@ -30,6 +34,7 @@ public class ScanTileService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
+        Log.d("QSTILE", "onClick called!");
         Intent intent = new Intent();
         intent.setClassName(
             "com.termux",
@@ -37,7 +42,8 @@ public class ScanTileService extends TileService {
         );
         intent.setAction("com.termux.RUN_COMMAND");
         intent.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/scripts/scan_wrapper.sh");
-        intent.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
+        intent.putExtra("com.termux.RUN_COMMAND_BACKGROUND", true);
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
         startService(intent);
     }
 }
