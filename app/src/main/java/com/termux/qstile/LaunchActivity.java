c2394648fba510e618ea1e45e36fbd2355039386
AlexBaldwinBin <oob6gg3@gmail.com>
2026-03-10 16:44:40 +0200

    Update LaunchActivity.java

--- app/src/main/java/com/termux/qstile/LaunchActivity.java
+++ app/src/main/java/com/termux/qstile/LaunchActivity.java
@@ -1,23 +1,21 @@
 package com.termux.qstile;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent();
-        intent.setClassName("com.termux", "com.termux.app.RunCommandService");
-        intent.setAction("com.termux.RUN_COMMAND");
-        intent.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/scripts/scan_wrapper.sh");
-        intent.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
-        intent.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", 0);
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        new Handler().postDelayed(() -> finish(), 2000);
+        try {
+            Runtime.getRuntime().exec(new String[]{
+                "/data/data/com.termux/files/usr/bin/bash",
+                "-c",
+                "/data/data/com.termux/files/home/scripts/scan_wrapper.sh"
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finish();
     }
 }
