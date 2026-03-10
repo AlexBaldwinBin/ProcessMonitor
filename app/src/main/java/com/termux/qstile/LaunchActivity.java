c0e31320d2c9d2ea58112821cd19f5e131a6196b
AlexBaldwinBin <oob6gg3@gmail.com>
2026-03-10 17:00:01 +0200

    Update LaunchActivity.java

--- app/src/main/java/com/termux/qstile/LaunchActivity.java
+++ app/src/main/java/com/termux/qstile/LaunchActivity.java
@@ -1,25 +1,25 @@
 package com.termux.qstile;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent();
-        intent.setClassName("com.termux", "com.termux.app.RunCommandService");
-        intent.setAction("com.termux.RUN_COMMAND");
-        intent.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/scripts/scan_wrapper.sh");
-        intent.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
-        intent.putExtra("com.termux.RUN_COMMAND_SESSION_ACTION", 0);
-        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent);
-        } else {
-            startService(intent);
+        try {
+            Runtime.getRuntime().exec(new String[]{
+                "/system/bin/am",
+                "startservice",
+                "-n", "com.termux/.app.RunCommandService",
+                "-a", "com.termux.RUN_COMMAND",
+                "--es", "com.termux.RUN_COMMAND_PATH",
+                "/data/data/com.termux/files/home/scripts/scan_wrapper.sh",
+                "--ez", "com.termux.RUN_COMMAND_BACKGROUND", "false",
+                "--ei", "com.termux.RUN_COMMAND_SESSION_ACTION", "0"
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         finish();
     }
