07ae5709ad08ba1f3e88b0c3075f4092caec020c
AlexBaldwinBin <oob6gg3@gmail.com>
2026-03-10 15:00:28 +0200

    Update ScanTileService.java

--- .github/workflows/app/app/src/main/app/src/main/java/com/termux/qstile/ScanTileService.java
+++ .github/workflows/app/app/src/main/app/src/main/java/com/termux/qstile/ScanTileService.java
@@ -1,20 +1,44 @@
 package com.termux.qstile;
 
+import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import android.content.Intent;
+import android.util.Log;
 
 public class ScanTileService extends TileService {
 
     @Override
+    public void onStartListening() {
+        super.onStartListening();
+        Tile tile = getQsTile();
+        if (tile != null) {
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.setLabel("Scan");
+            tile.updateTile();
+        }
+    }
+
+    @Override
+    public void onTileAdded() {
+        super.onTileAdded();
+        Tile tile = getQsTile();
+        if (tile != null) {
+            tile.setState(Tile.STATE_ACTIVE);
+            tile.updateTile();
+        }
+    }
+
+    @Override
     public void onClick() {
         super.onClick();
+        Log.d("QSTILE", "onClick called!");
         Intent intent = new Intent();
         intent.setClassName(
             "com.termux",
             "com.termux.app.RunCommandService"
         );
         intent.setAction("com.termux.RUN_COMMAND");
-        intent.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/scripts/scan.sh");
+        intent.putExtra("com.termux.RUN_COMMAND_PATH", "/data/data/com.termux/files/home/scripts/scan_wrapper.sh");
         intent.putExtra("com.termux.RUN_COMMAND_BACKGROUND", false);
         startService(intent);
     }
